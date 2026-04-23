79c28767cce8749bcc1e52d53072326bd3dccc28
gunnar <gunnarfornaziergomes@gmail.com>
2026-04-23 15:02:09 -0300

    carrinho

--- apps/carrinho/views.py
+++ apps/carrinho/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Carrinho, ItemCarrinho
+from produtos.models import Produto
 
-# Create your views here.
+@login_required
+def adicionar_ao_carrinho(request, produto_id):
+    produto = get_object_or_404(Produto, id=produto_id, disponivel=True)
+    carrinho, criado = Carrinho.objects.get_or_create(usuario=request.user, finalizado=False)
+    
+    item_carrinho, criado_item = ItemCarrinho.objects.get_or_create(carrinho=carrinho, produto=produto)
+    
+    if not criado_item:
+        item_carrinho.quantidade += 1
+        item_carrinho.save()
+    
+    messages.success(request, f'Produto "{produto.nome}" adicionado ao carrinho.')
+    return redirect('produtos:detalhes_produto', slug=produto.slug)
